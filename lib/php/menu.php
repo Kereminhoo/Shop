02cb86a3b1df9519ead3d272f74f74e1decb0172
Kereminhoo <kerem.icel@condorcet.be>
2024-09-26 14:17:17 +0200

    commit initial

---
+++ lib/php/menu.php
@@ -0,0 +1,19 @@
+
+
+<ul class="nav justify-content-center">
+    <li class="nav-item">
+        <a class="nav-link" href="index.php?page=accueil">Accueil</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="index.php?page=page1">Page 1</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="index.php?page=page2">Page 2</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="index.php?page=page3">Page 3</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="index.php?page=page4">Page 4</a>
+    </li>
+</ul>
